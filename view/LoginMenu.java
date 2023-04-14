d6271aee970ac6e238cf27dd7380b07d9d9b80c2
ma1eck <amir.hossein.maleck.mohammady@gmail.com>
2023-04-14 12:33:36 +0430

    clean LoginMenu and MapMenu

--- view/LoginMenu.java
+++ view/LoginMenu.java
@@ -1,50 +1,47 @@
 package view;
+
 import controller.LoginMenuCommands;
 import controller.Messages;
 import model.CaptchaPrinter;
 import controller.LoginMenuController;
 import controller.Controller;
+
 import java.io.*;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 import java.util.regex.*;
+
 public class LoginMenu {
     private static Scanner scanner;
     private LoginMenuController loginMenuController = new LoginMenuController();
-    public void run(Scanner scanner) throws IOException, NoSuchAlgorithmException{
+
+    public void run(Scanner scanner) throws IOException, NoSuchAlgorithmException {
         LoginMenu.scanner = scanner;
-        while(true){
+        while (true) {
             String input = scanner.nextLine();
-            if(LoginMenuCommands.getMatcher(input, LoginMenuCommands.CREATE_USER)!=null && createUserFormat(input))
-                System.out.println(createUser(input, scanner));
-            else if(LoginMenuCommands.getMatcher(input, LoginMenuCommands.USER_LOGIN)!=null){
+            if (LoginMenuCommands.getMatcher(input, LoginMenuCommands.CREATE_USER) != null && createUserFormat(input))
+                System.out.println(createUser(input));
+            else if (LoginMenuCommands.getMatcher(input, LoginMenuCommands.USER_LOGIN) != null) {
                 System.out.println(userLogin(input));
-                if(userLogin(input).equals("Login successful!"))
+                if (userLogin(input).equals("Login successful!"))
                     new MainMenu().run(scanner);
-            }
-            else if(LoginMenuCommands.getMatcher(input, LoginMenuCommands.FORGOT_MY_PASSWORD)!=null)
+            } else if (LoginMenuCommands.getMatcher(input, LoginMenuCommands.FORGOT_MY_PASSWORD) != null)
                 System.out.println(forgotMyPassword(scanner));
             else
                 System.out.println("Invalid command!");
         }
     }
 
-    private String createUser(String input,Scanner scanner) throws IOException, NoSuchAlgorithmException{
-        String username = Controller.trimmer(LoginMenuCommands.getMatcher(input, LoginMenuCommands.USERNAME).group("username"));
-        String password = "" , passwordConfirm = "";
-        if(LoginMenuCommands.getMatcher(input, LoginMenuCommands.PASSWORD).group("random")!=null) password = "random";
-        else{
-            password = Controller.trimmer(LoginMenuCommands.getMatcher(input, LoginMenuCommands.PASSWORD).group("password"));
-            passwordConfirm = Controller.trimmer(LoginMenuCommands.getMatcher(input, LoginMenuCommands.PASSWORD).group("passwordConfirm"));
-        }
-        String nickname = Controller.trimmer(LoginMenuCommands.getMatcher(input, LoginMenuCommands.NICKNAME).group("nickname"));
-        String email = Controller.trimmer(LoginMenuCommands.getMatcher(input, LoginMenuCommands.EMAIL).group("email"));
-        String slogan="";
-        if(LoginMenuCommands.getMatcher(input, LoginMenuCommands.SLOGAN)!=null){
-            slogan = Controller.trimmer(LoginMenuCommands.getMatcher(input, LoginMenuCommands.SLOGAN).group("slogan"));
-            if(slogan.length()==0) return "there's an empty field!";
-        }
-        switch (loginMenuController.signUp(username,password,passwordConfirm,email,slogan,nickname)) {
+    private String createUser(String input) throws IOException, NoSuchAlgorithmException {
+        String username = extractUsername(input);
+        String password = extractPassword(input);
+        String passwordConfirm = extractPasswordConfirm(input);
+        String nickname = extractNickname(input);
+        String email = extractEmail(input);
+        String slogan = extractSlogan(input);
+        if (slogan == null) return "there's an empty field!";
+
+        switch (loginMenuController.signUp(username, password, passwordConfirm, email, slogan, nickname)) {
             case EMPTY_FIELD:
                 return "There's is an empty field!";
             case INVALID_USERNAME:
@@ -60,7 +57,11 @@ public class LoginMenu {
                 String lower = "2. At least one lower case letter.";
                 String digit = "3. At least one digit.";
                 String nonWord = "4. At least one non-letter and non-digit character.";
-                return "Your password must contain the following characters:\n"+upper+"\n"+lower+"\n"+digit+"\n"+nonWord;
+                return "Your password must contain the following characters:\n"
+                        + upper + "\n"
+                        + lower + "\n"
+                        + digit + "\n"
+                        + nonWord;
             case EMAIL_EXISTS:
                 return "There's already a user with this email address!";
             case INVALID_EMAIL_FORMAT:
@@ -78,14 +79,58 @@ public class LoginMenu {
         }
         return null;
     }
-    private String userLogin(String input) throws IOException ,NoSuchAlgorithmException{
+
+    private String extractUsername(String input) {
+        Matcher usernameMatcher = LoginMenuCommands.getMatcher(input, LoginMenuCommands.USERNAME);
+        String username =
+                Controller.trimmer(usernameMatcher.group("username"));
+        return username;
+    }
+
+    private String extractPasswordConfirm(String input) {
+        Matcher passwordMatcher = LoginMenuCommands.getMatcher(input, LoginMenuCommands.PASSWORD);
+        if (passwordMatcher.group("random") != null) return "random";
+        else return Controller.trimmer(passwordMatcher.group("password"));
+    }
+
+    private String extractPassword(String input) {
+        Matcher passwordMatcher = LoginMenuCommands.getMatcher(input, LoginMenuCommands.PASSWORD);
+        if (passwordMatcher.group("random") != null) return "";
+        else return Controller.trimmer(passwordMatcher.group("passwordConfirm"));
+
+    }
+
+    private String extractNickname(String input) {
+        Matcher nicknameMatcher = LoginMenuCommands.getMatcher(input, LoginMenuCommands.NICKNAME);
+        String nickname = Controller.trimmer(nicknameMatcher.group("nickname"));
+        return nickname;
+    }
+
+    private String extractEmail(String input) {
+        Matcher emailMatcher = LoginMenuCommands.getMatcher(input, LoginMenuCommands.EMAIL);
+        String email = Controller.trimmer(emailMatcher.group("email"));
+        return email;
+    }
+
+    private String extractSlogan(String input) {
+        String slogan = "";
+        if (LoginMenuCommands.getMatcher(input, LoginMenuCommands.SLOGAN) != null) {
+            Matcher sloganMarcher = LoginMenuCommands.getMatcher(input, LoginMenuCommands.SLOGAN);
+            slogan = Controller.trimmer(sloganMarcher.group("slogan"));
+            if (slogan.length() == 0) return null;
+        }
+        return slogan;
+    }
+
+    private String userLogin(String input) throws IOException, NoSuchAlgorithmException {
         String username = Controller.trimmer(LoginMenuCommands.getMatcher(input, LoginMenuCommands.USERNAME).group("username"));
         String password = Controller.trimmer(LoginMenuCommands.getMatcher(input, LoginMenuCommands.PASSWORD_LOGIN).group("password"));
         switch (loginMenuController.login(username, password, false)) {
             case USERNAME_NOT_FOUND:
                 return "Username and password didn't match!";
             case WAIT_FOR_LOGIN:
-                return "you have to wait for "+Controller.getUserByUsername(username).getAttempt()*5+" more seconds to login!";
+                return "you have to wait for " +
+                        Controller.getUserByUsername(username).getAttempt() * 5 + " more seconds to login!";
             case INCORRECT_PASSWORD:
                 return "Incorrect password!";
             case EXIT_CAPTCHA:
@@ -97,39 +142,43 @@ public class LoginMenu {
         }
         return null;
     }
-    public static boolean doYouWantSuggestedUsername(String suggestedUsername){
-        System.out.println("this username already exists! here is a valid username: " + suggestedUsername +"\nenter yes to continue:");
+
+    public static boolean doYouWantSuggestedUsername(String suggestedUsername) {
+        System.out.println("this username already exists! here is a valid username: " + suggestedUsername
+                + "\nenter yes to continue:");
         return LoginMenu.scanner.nextLine().equals("yes");
     }
-    public static String confirmeRandomPassword(String randomPassword){
-        System.out.println("Your random password is: "+ randomPassword);
+
+    public static String confirmeRandomPassword(String randomPassword) {
+        System.out.println("Your random password is: " + randomPassword);
         System.out.println("Please re-enter your password here:");
         return LoginMenu.scanner.nextLine();
     }
-    public static String pickRecoveryQuestion(){
+
+    public static String pickRecoveryQuestion() {
         String questionInput = "";
-        while(LoginMenuCommands.getMatcher(questionInput, LoginMenuCommands.QUESTION_PICK)==null)
+        while (LoginMenuCommands.getMatcher(questionInput, LoginMenuCommands.QUESTION_PICK) == null)
             questionInput = LoginMenu.scanner.nextLine();
         return questionInput;
     }
 
-    private boolean createUserFormat(String input){
-        if(LoginMenuCommands.getMatcher(input, LoginMenuCommands.USERNAME)==null) return false;
-        if(LoginMenuCommands.getMatcher(input, LoginMenuCommands.PASSWORD)==null) return false;
-        if(LoginMenuCommands.getMatcher(input, LoginMenuCommands.EMAIL)==null) return false;
-        if(LoginMenuCommands.getMatcher(input, LoginMenuCommands.NICKNAME)==null) return false;
+    private boolean createUserFormat(String input) {
+        if (LoginMenuCommands.getMatcher(input, LoginMenuCommands.USERNAME) == null) return false;
+        if (LoginMenuCommands.getMatcher(input, LoginMenuCommands.PASSWORD) == null) return false;
+        if (LoginMenuCommands.getMatcher(input, LoginMenuCommands.EMAIL) == null) return false;
+        if (LoginMenuCommands.getMatcher(input, LoginMenuCommands.NICKNAME) == null) return false;
         return true;
     }
-    private String forgotMyPassword(Scanner scanner) throws IOException, NoSuchAlgorithmException{
+
+    private String forgotMyPassword(Scanner scanner) throws IOException, NoSuchAlgorithmException {
         System.out.println("please enter your username and new password:");
         String input = scanner.nextLine();
-        String username,password;
-        if (LoginMenuCommands.getMatcher(input, LoginMenuCommands.CHANGE_PASSWORD)!=null){
+        String username, password;
+        if (LoginMenuCommands.getMatcher(input, LoginMenuCommands.CHANGE_PASSWORD) != null) {
             username = LoginMenuCommands.getMatcher(input, LoginMenuCommands.USERNAME).group("username");
             password = LoginMenuCommands.getMatcher(input, LoginMenuCommands.PASSWORD_LOGIN).group("password");
-        }
-        else return "Invalid format!";
-        switch(loginMenuController.forgotPassword(username,password)) {
+        } else return "Invalid format!";
+        switch (loginMenuController.forgotPassword(username, password)) {
             case USERNAME_NOT_FOUND:
                 return "Username not found!";
             case INCORRECT_ANSWER:
@@ -141,28 +190,34 @@ public class LoginMenu {
                 String lower = "2. At least one lower case letter.";
                 String digit = "3. At least one digit.";
                 String nonWord = "4. At least one non-letter and non-digit character.";
-                return "Your password must contain the following characters:\n"+upper+"\n"+lower+"\n"+digit+"\n"+nonWord;
+                return "Your password must contain the following characters:\n"
+                        + upper + "\n"
+                        + lower + "\n"
+                        + digit + "\n"
+                        + nonWord;
             case PASSWORD_NOT_CONFIRMED:
                 return "Passwords do not match!";
             default:
-            break;
+                break;
         }
         return "Password changed successfully!";
     }
+
     public static String getPasswordRecoveryAnswer(String recoveryQuestion) {
         System.out.println(recoveryQuestion);
         return LoginMenu.scanner.nextLine();
     }
-    public static boolean checkCaptcha(){
+
+    public static boolean checkCaptcha() {
         CaptchaPrinter captchaPrinter = new CaptchaPrinter();
-        while (true){
+        while (true) {
             String captcha = Controller.generateCaptcha(captchaPrinter);
             System.out.println("enter the security code :");
             String input = LoginMenu.scanner.nextLine();
-            if(input.equals("cancel")) return false;
-            else if(input.equals(captcha)) return true;
+            if (input.equals("cancel")) return false;
+            else if (input.equals(captcha)) return true;
         }
     }
 
-    
+
 }
--- view/MapMenu.java
+++ view/MapMenu.java
@@ -1,75 +1,83 @@
 package view;
+
 import controller.MapMenuController;
 import controller.MapMenuCommands;
 import controller.Controller;
+
 import java.util.Scanner;
 import java.util.ArrayList;
+
 import model.TypeOfPixel;
+
 public class MapMenu {
     private MapMenuController mapMenuController = new MapMenuController();
     private int x;
     private int y;
-    public MapMenu (int x, int y) {
+
+    public MapMenu(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
     public void run(Scanner scanner) {
         printMap(this.x, this.y);
-        while(true){
+        while (true) {
             String input = scanner.nextLine();
-            if(MapMenuCommands.getMatcher(input, MapMenuCommands.EXIT)!=null)
+            if (MapMenuCommands.getMatcher(input, MapMenuCommands.EXIT) != null)
                 return;
-            else if(MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_MAP)!=null)
+            else if (MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_MAP) != null)
                 moveMap(input);
-            else if(MapMenuCommands.getMatcher(input, MapMenuCommands.SHOW_DETAILS)!=null)
+            else if (MapMenuCommands.getMatcher(input, MapMenuCommands.SHOW_DETAILS) != null)
                 System.out.println(showDetails(input));
-            else if(MapMenuCommands.getMatcher(input, MapMenuCommands.MAP_GUIDE)!=null)
+            else if (MapMenuCommands.getMatcher(input, MapMenuCommands.MAP_GUIDE) != null)
                 mapGuide();
             else
                 System.out.println("Invalid command!");
         }
     }
-    private void moveMap(String input){
-        int up = 0 , down = 0 , left = 0 , right = 0;
-        if(MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_UP)!=null){
+
+    private void moveMap(String input) {
+        int up = 0, down = 0, left = 0, right = 0;
+        if (MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_UP) != null) {
             String upString = MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_UP).group("up");
             up = (upString == null) ? 1 : -Integer.parseInt(upString);
         }
-        if(MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_DOWN)!=null){
+        if (MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_DOWN) != null) {
             String downString = MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_DOWN).group("down");
             down = (downString == null) ? 1 : Integer.parseInt(downString);
         }
-        if(MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_LEFT)!=null){
+        if (MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_LEFT) != null) {
             String leftString = MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_LEFT).group("left");
             left = (leftString == null) ? 1 : -Integer.parseInt(leftString);
         }
-        if(MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_RIGHT)!=null){
+        if (MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_RIGHT) != null) {
             String rightString = MapMenuCommands.getMatcher(input, MapMenuCommands.MOVE_RIGHT).group("right");
             right = (rightString == null) ? 1 : Integer.parseInt(rightString);
         }
-        if(!mapMenuController.checkCordinates(this.x, this.y, up, down, left, right))
+        if (!mapMenuController.checkCordinates(this.x, this.y, up, down, left, right))
             System.out.println("Cordinates out of bounds or invalid!");
-        else{
+        else {
             this.x += up + down;
             this.y += left + right;
-            printMap(this.x , this.y);
+            printMap(this.x, this.y);
         }
     }
-    private void printMap(int x , int y){
+
+    private void printMap(int x, int y) {
         ArrayList<ArrayList<Colors>> mapColorList = mapMenuController.getMapColorList(x, y);
         int numberOfRows = mapColorList.size();
         int numberOfColumns = mapColorList.get(0).size();
         int numberOfRowSplitters = numberOfColumns * 7 + 1;
-        for(int i = 0; i <= 4 * numberOfRows; i++){
-            if(i%4 == 0){
-                splitRow(numberOfRowSplitters, i!=0 && i!=4 * numberOfRows);
+        for (int i = 0; i <= 4 * numberOfRows; i++) {
+            if (i % 4 == 0) {
+                splitRow(numberOfRowSplitters, i != 0 && i != 4 * numberOfRows);
                 continue;
             }
-            for(int j = 0; j <= 7 * numberOfColumns; j++){
-                if(j%7 == 0)System.out.print("|");
-                else{
-                    int row = (int)Math.floor(i/4);
-                    int column = (int)Math.floor(j/7);
+            for (int j = 0; j <= 7 * numberOfColumns; j++) {
+                if (j % 7 == 0) System.out.print("|");
+                else {
+                    int row = (int) Math.floor(i / 4);
+                    int column = (int) Math.floor(j / 7);
                     System.out.print(mapColorList.get(row).get(column));
                     System.out.print("#");
                     System.out.print(Colors.RESET);
@@ -78,22 +86,24 @@ public class MapMenu {
             System.out.println();
         }
     }
-    private void splitRow(int length , boolean flag){
-        if(flag){
+
+    private void splitRow(int length, boolean flag) {
+        if (flag) {
             length -= 2;
             System.out.print("|");
-            while(length-- > 0)System.out.print("-");
+            while (length-- > 0) System.out.print("-");
             System.out.println("|");
-        }
-        else {
-            while(length-- > 0)System.out.print("-");
+        } else {
+            while (length-- > 0) System.out.print("-");
             System.out.println();
         }
     }
-    private String showDetails(String input){
+
+    private String showDetails(String input) {
         return null;
     }
-    private void mapGuide(){
+
+    private void mapGuide() {
         printGuide(TypeOfPixel.LAND);
         printGuide(TypeOfPixel.PEBBLE);
         printGuide(TypeOfPixel.ROCK);
@@ -102,7 +112,8 @@ public class MapMenu {
         printGuide(TypeOfPixel.FIELD);
         printGuide(TypeOfPixel.MEADOW);
     }
-    private void printGuide(TypeOfPixel typeOfPixel){
+
+    private void printGuide(TypeOfPixel typeOfPixel) {
         System.out.print(TypeOfPixel.getColor(typeOfPixel));
         System.out.print(" ");
         System.out.print(Colors.RESET);
