e87b90da2c0de05adb234f3d0a23b4ec87577ce7
LordHajAli <anajar13750@gmail.com>
2023-06-30 18:22:13 +0330

    friend

--- Stronghold crusader/Server/Connection.java
+++ Stronghold crusader/Server/Connection.java
@@ -18,6 +18,7 @@ public class Connection extends Thread {
         this.socket = socket;
         this.dataInputStream = new DataInputStream(socket.getInputStream());
         this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        connections.add(this);
     }
 
     @Override
@@ -32,36 +33,34 @@ public class Connection extends Thread {
         try {
             dataOutputStream.writeUTF("entered main menu");
             while (true) {
-                String input = dataInputStream.readUTF();
-                if (input.equals("enter global chat"))
-                    new GlobalChatMenu(dataOutputStream, dataInputStream, currentUser).globalChat();
-                else if (input.equals("enter private chat"))
-                    new PrivateChatMenu(dataOutputStream, dataInputStream, currentUser).privateChat();
-                else if (input.equals("enter group chat"))
-                    new GroupChatMenu(dataOutputStream, dataInputStream, currentUser).GroupChat();
-                else if (input.matches("\\s*logout\\s*")) return;
-                else dataOutputStream.writeUTF("invalid input");
+                if(dataInputStream.available()!=0) {
+                    System.out.println("KIr");
+                    String input = dataInputStream.readUTF();
+                    if (input.matches("\\s*friendRequest (\\w+)\\s*")) {
+                        dataOutputStream.writeUTF(sendFriendRequest(input));
+                        User.updateUsers();
+                    } else if (input.matches("\\s*acceptFriend (\\w+)\\s*")) {
+                        dataOutputStream.writeUTF(acceptFriendRequest(input));
+                        User.updateUsers();
+                    } else if (input.matches("\\s*friendReject (\\w+)\\s*")) {
+                        dataOutputStream.writeUTF(rejectFriendRequest(input));
+                        User.updateUsers();
+                    } else if (input.matches("\\s*showDetails (\\w+)\\s*")) {
+                        dataOutputStream.writeUTF(showFriendDetails(input));
+                        User.updateUsers();
+                    }
+                    else if (input.equals("enter global chat"))
+                        new GlobalChatMenu(dataOutputStream, dataInputStream, currentUser).globalChat();
+                    else if (input.equals("enter private chat"))
+                        new PrivateChatMenu(dataOutputStream, dataInputStream, currentUser).privateChat();
+                    else if (input.equals("enter group chat"))
+                        new GroupChatMenu(dataOutputStream, dataInputStream, currentUser).GroupChat();
+                    else if (input.matches("\\s*logout\\s*")) return;
+                    else dataOutputStream.writeUTF("invalid input");
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    private synchronized void handleClient() throws IOException {
-        if(dataInputStream.available()!=0){
-            String input = dataInputStream.readUTF();
-            if(input.matches("\\s*acceptFriend (\\w+)\\s*")){
-                dataOutputStream.writeUTF(sendFriendRequest(input));
-            }
-            else if(input.matches("\\s*friendRequest (\\w+)\\s*")){
-                dataOutputStream.writeUTF(acceptFriendRequest(input));
-            }
-            else if(input.matches("\\s*friendReject (\\w+)\\s*")) {
-                dataOutputStream.writeUTF(rejectFriendRequest(input));
-            }
-            else if(input.matches("\\s*showDetails (\\w+)++s*")) {
-                dataOutputStream.writeUTF(showFriendDetails(input));
-            }
         }
     }
 
@@ -82,17 +81,18 @@ public class Connection extends Thread {
 
     public synchronized String sendFriendRequest(String friendRequest) throws IOException {
         String[] request = friendRequest.split(" ");
+        System.out.println(request.length+" "+request[0]+" "+request[1]);
         if(request.length!=2 || !request[0].equals("friendRequest"))
             return "invalid command!";
         else{
             String targetUser = request[1];
             Connection connection = getConnectionByUsername(targetUser);
-            if(connection==null){
+            if(connection==null || targetUser.equals(currentUser.getUsername())){
                 return "invalid target!";
             }
             if(currentUser.getFriends().contains(targetUser))
                 return "this user is already your friend!";
-            if(connection.currentUser.getPendingFriendRequests().contains(targetUser))
+            if(connection.currentUser.getPendingFriendRequests().contains(currentUser.getUsername()))
                 return "you have already sent a request to this user!";
             connection.dataOutputStream.writeUTF("acceptFriend? "+currentUser.getUsername());
             connection.currentUser.addFriendRequest(currentUser.getUsername());
@@ -102,12 +102,12 @@ public class Connection extends Thread {
 
     public synchronized String acceptFriendRequest(String acceptRequest) throws IOException {
         String[] accept = acceptRequest.split(" ");
-        if(accept.length!=2 || !accept[0].equals("friendAccept"))
+        if(accept.length!=2 || !accept[0].equals("acceptFriend"))
             return "invalid command!";
         else{
             String targetUser = accept[1];
             Connection connection = getConnectionByUsername(targetUser);
-            if(connection==null){
+            if(connection==null || targetUser.equals(currentUser.getUsername())){
                 return "invalid target!";
             }
             if(!currentUser.getPendingFriendRequests().contains(targetUser))
@@ -126,7 +126,7 @@ public class Connection extends Thread {
         else{
             String targetUser = reject[1];
             Connection connection = getConnectionByUsername(targetUser);
-            if(connection==null){
+            if(connection==null || targetUser.equals(currentUser.getUsername())){
                 return "invalid target!";
             }
             if(!currentUser.getPendingFriendRequests().contains(targetUser))
